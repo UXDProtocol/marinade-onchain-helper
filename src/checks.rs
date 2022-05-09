032f5cd3519522eb20db548374077c4732c8645b
cnek <ctk2012ac@gmail.com>
2022-05-09 15:49:02 +0800

    fix upgrade and added marinade state

---
+++ src/checks.rs
@@ -0,0 +1,115 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, TokenAccount};
+
+use crate::error::CommonError;
+
+pub fn check_min_amount(amount: u64, min_amount: u64, action_name: &str) -> Result<()> {
+    if amount >= min_amount {
+        Ok(())
+    } else {
+        msg!(
+            "{}: Number too low {} (min is {})",
+            action_name,
+            amount,
+            min_amount,
+        );
+        Err(CommonError::NumberTooLow.into())
+    }
+}
+
+pub fn check_address(
+    actual_address: &Pubkey,
+    reference_address: &Pubkey,
+    field_name: &str,
+) -> Result<()> {
+    if actual_address == reference_address {
+        Ok(())
+    } else {
+        msg!(
+            "Invalid {} address: expected {} got {}",
+            field_name,
+            reference_address,
+            actual_address
+        );
+        Err(ProgramError::InvalidArgument.into())
+    }
+}
+
+pub fn check_owner_program<'info, A: ToAccountInfo<'info>>(
+    account: &A,
+    owner: &Pubkey,
+    field_name: &str,
+) -> Result<()> {
+    let actual_owner = account.to_account_info().owner;
+    if actual_owner == owner {
+        Ok(())
+    } else {
+        msg!(
+            "Invalid {} owner_program: expected {} got {}",
+            field_name,
+            owner,
+            actual_owner
+        );
+        Err(ProgramError::InvalidArgument.into())
+    }
+}
+
+pub fn check_mint_authority(mint: &Mint, mint_authority: Pubkey, field_name: &str) -> Result<()> {
+    if mint.mint_authority.contains(&mint_authority) {
+        Ok(())
+    } else {
+        msg!(
+            "Invalid {} mint authority {}. Expected {}",
+            field_name,
+            mint.mint_authority.unwrap_or_default(),
+            mint_authority
+        );
+        Err(ProgramError::InvalidAccountData.into())
+    }
+}
+
+pub fn check_freeze_authority(mint: &Mint, field_name: &str) -> Result<()> {
+    if mint.freeze_authority.is_none() {
+        Ok(())
+    } else {
+        msg!("Mint {} must have freeze authority not set", field_name);
+        Err(ProgramError::InvalidAccountData.into())
+    }
+}
+
+pub fn check_mint_empty(mint: &Mint, field_name: &str) -> Result<()> {
+    if mint.supply == 0 {
+        Ok(())
+    } else {
+        msg!("Non empty mint {} supply: {}", field_name, mint.supply);
+        Err(ProgramError::InvalidArgument.into())
+    }
+}
+
+pub fn check_token_mint(token: &TokenAccount, mint: Pubkey, field_name: &str) -> Result<()> {
+    if token.mint == mint {
+        Ok(())
+    } else {
+        msg!(
+            "Invalid token {} mint {}. Expected {}",
+            field_name,
+            token.mint,
+            mint
+        );
+        Err(ProgramError::InvalidAccountData.into())
+    }
+}
+
+pub fn check_token_owner(token: &TokenAccount, owner: &Pubkey, field_name: &str) -> Result<()> {
+    if token.owner == *owner {
+        Ok(())
+    } else {
+        msg!(
+            "Invalid token account {} owner {}. Expected {}",
+            field_name,
+            token.owner,
+            owner
+        );
+        Err(ProgramError::InvalidAccountData.into())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,28 @@
+use anchor_lang::{AnchorSerialize, AnchorDeserialize};
+use anchor_lang::prelude::*;
+
 pub mod cpi_context_accounts;
 pub mod cpi_util;
+pub mod state;
+pub mod stake_system;
+pub mod validator_system;
+pub mod liq_pool;
+pub mod calc;
+pub mod error;
+pub mod located;
+pub mod list;
+pub mod checks;
+
+/// The static program ID
+pub static ID: Pubkey = Pubkey::new_from_array([
+    5, 69, 227, 101, 190, 242, 113, 173, 117, 53, 3, 103, 86, 93, 164, 13, 163, 54, 220, 28, 135,
+    155, 177, 84, 138, 122, 252, 197, 90, 169, 57, 30,
+]); // "MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD"
+
+//-----------------------------------------------------
+#[derive(
+    Clone, Copy, Debug, Default, AnchorSerialize, AnchorDeserialize, PartialEq, Eq, PartialOrd, Ord,
+)]
+pub struct Fee {
+    pub basis_points: u32,
+}
